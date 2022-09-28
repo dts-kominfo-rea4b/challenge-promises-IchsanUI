aa592c06f8c96e9cbb6e7333c6c6794eb48bc57b
Mochammad Ichsan <mochammadichsan02@gmail.com>
2022-09-28 15:32:20 +0700

    Menyelesaikan Challage Promise dan CallBack

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi,namaFilm) => {
+  try {
+    const TheaterIXX = await promiseTheaterIXX();
+    const TheaterVGC = await promiseTheaterVGC();
+    let result = 0;
+    return new Promise((resolve) => {
+      result += TheaterVGC.filter((item) => item.hasil == emosi).length;
+      result += TheaterIXX.filter((item) => item.hasil == emosi).length;
+      resolve(result);
+    });
+
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = {
   promiseOutput,
